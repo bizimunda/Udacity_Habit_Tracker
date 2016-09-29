fe29fdeb196b6ca019b89764d95400e0429c358e
bizimunda <yourbelover@gmail.com>
2016-09-29 10:59:19 +0200

    added cursor to return in the read();

--- app/src/main/java/com/example/android/udacityhabittracker/MainActivity.java
+++ app/src/main/java/com/example/android/udacityhabittracker/MainActivity.java
@@ -5,8 +5,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,12 +24,17 @@ public class MainActivity extends ActionBarActivity {
 
         //displayView= (TextView) findViewById(R.id.tvDisplay);
 
-        //delete();
-        //insert();
-        //read();
-        //update();
-    }
 
+        //delete();
+
+        //insert();
+
+        //read();
+
+        //update();
+
+    }
+    //this method let us to insert data into the db
     private void insert(){
         String nameString="swimming";
         int count=3;
@@ -53,6 +56,7 @@ public class MainActivity extends ActionBarActivity {
         db.close();
     }
 
+    //this method let us to read data from the db
     private void read(){
         mDbHelper= new HabitDbHelper(this);
         SQLiteDatabase db=mDbHelper.getReadableDatabase();
@@ -76,11 +80,23 @@ public class MainActivity extends ActionBarActivity {
         try{
             //displayView.setText("Number of rows in this Habit Table are: " + cursor.getCount());
 
+            int idColumnIndex= cursor.getColumnIndex(HabitContract.HabitEntry._ID);
+            int nameColumnIndex= cursor.getColumnIndex(HabitContract.HabitEntry.COLUMN_HABIT_NAME);
+            int countColumnIndex= cursor.getColumnIndex(HabitContract.HabitEntry.COLUMN_HABIT_COUNT);
+
+            while (cursor.moveToNext()){
+                int currentID=cursor.getInt(idColumnIndex);
+                String currentName=cursor.getString(nameColumnIndex);
+                int currentCount=cursor.getInt(countColumnIndex);
+                Toast.makeText(this, "  " + currentID + currentName + currentCount, Toast.LENGTH_SHORT).show();
+            }
+
         } finally {
             cursor.close();
         }
     }
 
+    //this method let us to delete rows from the db
     private void delete(){
         mDbHelper= new HabitDbHelper(this);
         SQLiteDatabase db= mDbHelper.getWritableDatabase();
@@ -92,6 +108,8 @@ public class MainActivity extends ActionBarActivity {
         db.close();
     }
 
+
+    //this method let us to modify data from the db
     private void update(){
         mDbHelper= new HabitDbHelper(this);
         SQLiteDatabase db= mDbHelper.getWritableDatabase();
@@ -118,27 +136,5 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-    //region MenuOptions
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    //endregion
 }
